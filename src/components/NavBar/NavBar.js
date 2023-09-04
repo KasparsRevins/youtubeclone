fe6fcc0f2c7c3e118057bdc5cc87391767364a10
Kaspars Revins <okoikwedced@gmail.com>
2023-09-04 12:40:14 +0100

    MUI Library

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,47 +1,235 @@
-import SideBar from "../SideBar/SideBar";
-import "./style.css";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import { AspectRatio } from '@mui/icons-material';
 
-const NavBar = () => {
-  return (
-    <div>
-      <nav class="navbar sticky-top">
-        <div class="container-fluid">
-          <div>
-            <SideBar />
-            <a class="navbar-brand logo">
-              <img src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500" />
-            </a>
-          </div>
-          <div id="center">
-            <form class="d-flex" role="search">
-              <input
-                id="search-bar"
-                class="rounded-pill form-control border-dark rounded-0 rounded-end"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button
-                class="rounded-pill rounded-0 rounded-start btn btn-light btn-outline-dark"
-                type="submit"
-              >
-                <i class="bi bi-search"></i>
-              </button>
-            </form>
-          </div>
-          <div class="row flex-nowrap">
-            <div class="col-md-1 p-2">
-            <i class="bi bi-three-dots-vertical" type="button"></i>
-            </div>
-            <div class="mx-1 border rounded-pill p-1">
-              <div type="button" class="ml-2">
-                <i class="bi bi-person-circle"></i>Sign In
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'black',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
   );
-};
-export default NavBar;
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="black">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="black"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="black"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar style={{backgroundColor: "white"}} position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="black"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+            <img style={{width: "90px"}} src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500" />
+          </Typography>
+          <Search>
+            <StyledInputBase
+              style={{width:"250px", border: "1px solid grey", borderRadius: "25px 0 0 25px"}}
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              />
+              <SearchIconWrapper>
+              </SearchIconWrapper>
+              <SearchIcon style={{color: "grey", backgroundColor: "WhiteSmoke", border: "1px solid grey", height: "41px", borderRadius: "0 25px 25px 0"}}/>
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="black">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="black"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="black"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="black"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -19,66 +19,56 @@ const Video = () => {
           </div>
         </div>
         <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-      </div>     
+          <img
+            class="card-img-top rounded"
+            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Video name</h5>
+            <p class="text-left">View count</p>
+          </div>
+        </div>
+        <div class="card border-0" type="button">
+          <img
+            class="card-img-top rounded"
+            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Video name</h5>
+            <p class="text-left">View count</p>
+          </div>
+        </div>
+        <div class="card border-0" type="button">
+          <img
+            class="card-img-top rounded"
+            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Video name</h5>
+            <p class="text-left">View count</p>
+          </div>
+        </div>
+        <div class="card border-0" type="button">
+          <img
+            class="card-img-top rounded"
+            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Video name</h5>
+            <p class="text-left">View count</p>
+          </div>
+        </div>
+        <div class="card border-0" type="button">
+          <img
+            class="card-img-top rounded"
+            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Video name</h5>
+            <p class="text-left">View count</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
