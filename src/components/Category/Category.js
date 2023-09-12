487d09181ca9e32e4a9a2ef10a19afd1fd02ccc6
Kaspars Revins <okoikwedced@gmail.com>
2023-09-12 11:02:16 +0100

    Placeholder tabs

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,18 +1,27 @@
-import "./style.css";
-const Category = () => {
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+export default function CenteredTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div class="container" style={{ paddingTop: "25px" }}>
-      <div class="grid text-center align-items-center category">
-        <div class="border rounded-pill p-1" style={{color: "white", backgroundColor: "black"}} type="button">All</div>
-        <div class="border rounded-pill p-1" type="button">Adventure</div>
-        <div class="border rounded-pill p-1" type="button">Music</div>
-        <div class="border rounded-pill p-1" type="button">Live</div>
-        <div class="border rounded-pill p-1" type="button">Chill-out music</div>
-        <div class="border rounded-pill p-1" type="button">News</div>
-        <div class="border rounded-pill p-1" type="button">Background music</div>
-        <div class="border rounded-pill p-1" type="button">Gadgets</div>
-      </div>
-    </div>
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Adventure" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Music" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Live" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Chill-out music" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="News" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Background music" />
+        <Tab class="border rounded-pill p-1" style={{margin: 5}} label="Gadgets" />
+      </Tabs>
+    </Box>
   );
-};
-export default Category;
+}
+
