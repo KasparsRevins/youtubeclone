49d8c717f2f767f109d947528f0164afa4654a02
Kaspars Revins <okoikwedced@gmail.com>
2023-09-21 12:50:21 +0100

    Slight UI adjustments

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -7,12 +7,11 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import "./style.css";
 
 const Video = () => {
   return (
-    <Grid container spacing={4}>
-      <Grid item xs={4.2} sm={3.2} md={3} >
+    <Grid container spacing={4} padding={3} paddingRight={"10%"} paddingLeft={"10%"}  >
+      <Grid item xs={4.2} sm={3.2} md={3}>
         <Card sx={{ maxWidth: 345 }}>
           <CardActionArea>
             <CardMedia
@@ -20,14 +19,17 @@ const Video = () => {
               height="140"
               image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
               alt="green iguana"
+              sx={{borderRadius: "8px"}}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                Lizards News
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                10 mil views - 1 year ago
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -41,14 +43,17 @@ const Video = () => {
               height="140"
               image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
               alt="green iguana"
+              sx={{borderRadius: "8px"}}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                Lizards News
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                10 mil views - 1 year ago
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -62,14 +67,17 @@ const Video = () => {
               height="140"
               image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
               alt="green iguana"
+              sx={{borderRadius: "8px"}}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                Lizards News
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                10 mil views - 1 year ago
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -83,14 +91,17 @@ const Video = () => {
               height="140"
               image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
               alt="green iguana"
+              sx={{borderRadius: "8px"}}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                Lizards News
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                10 mil views - 1 year ago
               </Typography>
             </CardContent>
           </CardActionArea>
@@ -104,14 +115,17 @@ const Video = () => {
               height="140"
               image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
               alt="green iguana"
+              sx={{borderRadius: "8px"}}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 Lizard
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
+                Lizards News
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                10 mil views - 1 year ago
               </Typography>
             </CardContent>
           </CardActionArea>
