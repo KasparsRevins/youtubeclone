61e4e86d9fe0a7ffaa808876d1c082585200e0a3
Kaspars Revins <okoikwedced@gmail.com>
2023-09-21 12:30:27 +0100

    Placeholder lizard responsive layout

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,75 +1,123 @@
+import * as React from "react";
+import {
+  Card,
+  Grid,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import "./style.css";
 
 const Video = () => {
   return (
-    <div>
-      <div class="grid text-center container-fluid video-grid">
-        <div class="row">
-          <div class="col-md-3">
-            <div class="card border-0" type="button">
-              <img
-                class="card-img-top rounded"
-                src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-              />
-              <div class="card-body">
-                <h5 class="card-title">Video name</h5>
-                <p class="text-left">View count</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card border-0" type="button">
-          <img
-            class="card-img-top rounded"
-            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Video name</h5>
-            <p class="text-left">View count</p>
-          </div>
-        </div>
-        <div class="card border-0" type="button">
-          <img
-            class="card-img-top rounded"
-            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Video name</h5>
-            <p class="text-left">View count</p>
-          </div>
-        </div>
-        <div class="card border-0" type="button">
-          <img
-            class="card-img-top rounded"
-            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Video name</h5>
-            <p class="text-left">View count</p>
-          </div>
-        </div>
-        <div class="card border-0" type="button">
-          <img
-            class="card-img-top rounded"
-            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Video name</h5>
-            <p class="text-left">View count</p>
-          </div>
-        </div>
-        <div class="card border-0" type="button">
-          <img
-            class="card-img-top rounded"
-            src="https://images.pexels.com/photos/14453705/pexels-photo-14453705.jpeg?auto=compress&cs=tinysrgb&w=300"
-          />
-          <div class="card-body">
-            <h5 class="card-title">Video name</h5>
-            <p class="text-left">View count</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Grid container spacing={4}>
+      <Grid item xs={4.2} sm={3.2} md={3} >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={4.2} sm={3.2} md={3} >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={4.2} sm={3.2} md={3} >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={4.2} sm={3.2} md={3} >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+      <Grid item xs={4.2} sm={3.2} md={3} >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 export default Video;
